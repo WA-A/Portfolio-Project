dc4a76a9cd2d75375052df27140c1e506d7cfdd0
Wasan  Awwad <108180628+WA-A@users.noreply.github.com>
2024-02-10 20:00:54 +0200

    Button Scroll to Top

--- assest/js/main.js
+++ assest/js/main.js
@@ -3,8 +3,11 @@
 // When scroll change background for navbar depending on the offset y
 const navbar = document.querySelector('.navbar')
 const headerhello=document.querySelector('header_content span')
+const ScrolltoTop = document.querySelector('.scroll-to-top')
+const aboutsec = document.querySelector('.about')
+const navLinks=document.querySelectorAll('.nav-link')
 
-window.addEventListener('scroll',function(){          
+window.addEventListener('scroll',function(){   // type scroll        
   // console.log("wasan");
   console.log(window.scrollY);
   if(window.screenY > headerhello.offsetTop){
@@ -13,4 +16,38 @@ window.addEventListener('scroll',function(){
   else {
     navbar.style.background = "transparent"
   }
+  if(window.screenY > aboutsec.offsetTop){
+    ScrolltoTop.classList.remove('opacity-0','invisible')
+  }
+  else{
+    ScrolltoTop.classList.add('opacity-0','invisible')
+  }
 })
+// Loading 
+/*const loading =document.querySelector('.loading')
+window.addEventListener('load',function(){   // type load
+ setTimeout(function(){
+ loading.classList.add('opacity-0','invisible')
+ },2000)
+})*/
+
+// Button Scroll to Top
+
+ScrolltoTop.addEventListener('click',function(){ 
+    window.scroll({
+        top:0,
+        behavior:"smooth",
+    })
+})
+
+// scroll  on each section
+for(let i =0; i<navLinks.length; i++){
+navLinks[i].addEventListener('click',function(){
+let currentid = this.getAttribte('href')
+let targetsec = document.querySelector(currentid)
+  window.scroll({
+        top:targetsec.offsetTop,
+        behavior:"smooth",
+    })
+})
+}
